5166eff85ff7ee66f43382501dc0795fb77dbfab
AliceAlexander10 <alicemariealexander@gmail.com>
2024-04-07 11:57:11 +0200

    ajout dossier portfolio

---
+++ JS/menuburger.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.getElementById("toggleMenu");
+    if (menu.className === "menu") {
+      menu.className += "responsive";
+    } else {
+      menu.className = "menu";
+    }
+}
